8124fb1eae432025fe239bb163f9f6cc5f21e644
Sebastian <Sebastian@Strobl.net>
2020-06-22 14:54:21 +0200

    Update: Example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,26 +1,29 @@
 extern crate acute;
 
-use acute::acute::Acute;
-use acute::window::WinitState;
-use acute::scenes::Scene;
-
-use winit::{event::Event, event::WindowEvent, dpi::LogicalSize};
-use winit::event_loop::ControlFlow;
-use winit::window::WindowBuilder;
-use std::time::Duration;
-use legion::world::World;
-
-use legion::entity::Entity;
+use winit::{dpi::LogicalSize};
+use std::time::{Duration, Instant};
+use legion::prelude::*;
+use acute::{
+    acute::Acute,
+    window::WinitState,
+    scenes::Scene,
+    components::{
+        geometry::{Mesh, Cuboid, Quad, Triangle, Vertex},
+        default::{
+            Transform, Color, Static, Group
+        }
+    },
+};
 use ultraviolet::{
     Vec3,
     Rotor3,
 };
-use acute::components::simple::{Transform, Color};
-use acute::components::geometry::Triangle2D;
-
 
 const WINDOW_SIZE: LogicalSize<u32> = LogicalSize { width: 1280, height: 720 };
 const TITLE: &str = "Acute Test";
+
+#[derive(Copy, Clone, PartialEq)]
+struct UpDown;
 
 fn main() {
     let (window_builder, event_loop) = WinitState::new(TITLE, WINDOW_SIZE);
@@ -33,38 +36,107 @@ fn main() {
     });
 }
 
-struct TestScene {}
+struct TestScene {
+    time_since_start: Instant,
+}
 
 impl TestScene {
     fn new() -> Self {
-        Self {}
+        Self {
+            time_since_start: Instant::now()
+        }
     }
 }
 
 impl Scene for TestScene {
-    fn update(&mut self, world: &mut World, delta_time: &Duration) {}
+    fn update(&mut self, world: &mut World, delta_time: &Duration) {
 
-    fn fixed_update(&mut self, world: &mut World, delta_time: &Duration) {}
+    }
+
+    fn fixed_update(&mut self, world: &mut World, delta_time: &Duration) {
+        let move_query = <Write<Transform>>::query().filter(tag::<UpDown>());
+
+        for (mut transform) in move_query.iter_mut(world) {
+            transform.position[1] += self.time_since_start.elapsed().as_secs_f32().sin() * 0.01;
+        }
+
+        let rotate_xy_query = <Write<Transform>>::query().filter(tag_value(&Group("rotate_xy")));
+        for (mut transform) in rotate_xy_query.iter_mut(world) {
+            transform.rotation += Rotor3::from_rotation_xy(12.0*delta_time.as_secs_f32());
+            transform.rotation.normalize();
+        }
+
+        let rotate_xz_query = <Write<Transform>>::query().filter(tag_value(&Group("rotate_xyxz")));
+        for (mut transform) in rotate_xz_query.iter_mut(world) {
+            transform.rotation += Rotor3::from_rotation_xz(12.0*delta_time.as_secs_f32());
+            transform.rotation += Rotor3::from_rotation_xy(12.0*delta_time.as_secs_f32());
+            transform.rotation.normalize();
+        }
+
+        let mesh_query = <(Write<Mesh>, Read<Transform>)>::query().filter(changed::<Transform>());
+
+        for (mut mesh, transform) in mesh_query.iter_mut(world) {
+            mesh.update_rotation(transform.rotation);
+        }
+    }
 
     fn on_start(&mut self, world: &mut World) {
-        println!("init_world");
-        for i in 0..5 {
-            world.insert(
+        // create ground, and mark as static
+        // TODO: add scaling so one can be used
+        for i in -10..10 {
+            for j in -10..20 {
+                let entity = world.insert(
+                    (),
+                    std::iter::once((
+                        Transform {
+                            position: Vec3::new((i as f32), 0.0, (j as f32)),
+                            rotation: Rotor3::identity(),
+                        },
+                        Mesh::new_mesh(&Mesh::default_horizontal_quad().to_vec()),
+                        Color { data: [0.3, 0.5, 0.7, 1.0]}
+                    )))[0];
+
+                world.add_tag(entity, Static).unwrap();
+            }
+        }
+
+        // insert a cube with the up_down movement tag
+        {
+            let entity = world.insert(
                 (),
-                (0..1).map(|_| (
+                std::iter::once((
                     Transform {
-                        pos: Vec3::new((2 * i) as f32, 0.0, i as f32),
-                        scale: Vec3::default(),
-                        rotation: Rotor3 { s: 0.0, bv: Default::default() },
+                        position: Vec3::new(0.0, -2.0, 0.0),
+                        rotation: Rotor3::identity(),
                     },
-                    Triangle2D {
-                        a: Vec3::new((-i * 2) as f32, (-i * 2) as f32, i as f32),
-                        b: Vec3::new((i * 2) as f32, (-i * 2) as f32, i as f32),
-                        c: Vec3::new(0.0, (i * 2) as f32, 0.0),
+                    Mesh::new_mesh(&Mesh::default_cuboid().to_vec()),
+                    Color { data: [0.2, 0.8, 0.3, 1.0] }
+                )))[0];
+
+            world.add_tag(entity, UpDown).unwrap();
+            world.add_tag(entity, Group("rotate_xyxz")).unwrap();
+        }
+
+        // insert a quad with the rotate_xy tag
+        {
+            let entity = world.insert(
+                (),
+                std::iter::once((
+                    Transform {
+                        position: Vec3::new(1.0, -1.5, -4.0),
+                        rotation: Rotor3::identity(),
                     },
-                    Color { data: [1.0 / i as f32, 1.0 / (2 * i) as f32, 1.0 / ( 2 * i) as f32, 1.0]}
-                )),
-            );
+                    Mesh::new_mesh(&Mesh::default_quad().to_vec()),
+                    Color { data: [0.5, 0.4, 0.3, 1.0]}
+                )))[0];
+
+            world.add_tag(entity, Group("rotate_xy")).unwrap();
+        }
+
+        // rotate the vertices of each entity in the beginning for not identity cases
+        let rotation_query = <(Write<Mesh>, Read<Transform>)>::query();
+        for (mut mesh, transform) in rotation_query.iter_mut(world) {
+            mesh.update_rotation(transform.rotation);
         }
     }
 }
